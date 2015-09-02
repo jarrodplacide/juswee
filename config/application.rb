d9556fcb776d9844a760d73fb6f2972b88062056
Jarrod Placide-Raymond <royaltyclubvp@royalty-club.com>
2015-09-02 04:33:43 -0400

    Added Web Fonts

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module Juswee
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Add Font Path to Asset Pipeline
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
